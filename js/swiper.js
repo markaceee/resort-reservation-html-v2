365dabbcaec3533c354532e5530b2313a29313eb
Sova <boquiren40@gmail.com>
2023-04-16 18:37:45 +0800

    first commit

---
+++ js/swiper.js
@@ -0,0 +1,12 @@
+// import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.js'
+
+
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    loop: true,
+    centeredSlides: true,
+    autoplay: { 
+      delay: 5000,
+      disableOnInteraction: false,
+    }
+  });
